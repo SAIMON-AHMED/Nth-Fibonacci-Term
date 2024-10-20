344ec63d94d7e08fa2d9caaceb40f6a701d29171
Saimon Ahmed <67533318+SAIMON-AHMED@users.noreply.github.com>
2024-10-20 08:57:02 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
-// Time -> O(2^n) | Space -> O(n) - where n is the number of nodes
-
+// A better solution than recursive implementation regarding complexity.
+// Time -> O(n) | Space -> O(1) - where n is the number of nodes
 function getNthFib(n) {
 
   if (n === 1) return 0;
   if (n === 2) return 1;
 
-  return getNthFib(n - 1) + getNthFib(n - 2);
-  
+  let nthTerm = 0;
+  let firstTerm = 0;
+  let secondTerm = 1;
+
+  for (let i = 2; i < n; i++) {
+    let temp = secondTerm
+    secondTerm = firstTerm + secondTerm;
+    firstTerm = temp;
+  }
+  nthTerm = secondTerm;
+  return nthTerm;
+
 }
+
